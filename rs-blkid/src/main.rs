78fc21810b3fc04f149655ef3f5071ab853ba596
rysndavjd <138254890+rysndavjd@users.noreply.github.com>
2025-05-01 17:34:27 +0200

    u

--- rs-blkid/src/main.rs
+++ rs-blkid/src/main.rs
@@ -1,12 +1,16 @@
 use rs_libblkid::*;
-use rs_libblkid::Ext4SuperBlock;
+use uuid::Uuid;
 
 fn ext4_into() -> Result<(), Box<dyn std::error::Error>> {
     let device = read_ext4("/dev/sdb1")?;
-    println!("{:?}", device.s_inodes_count);
+    //println!("Raw values {:?}", device.s_uuid);
+    println!("UUID string: {:?}", device.s_state);
     return Ok(());
 }
 
 fn main() {
-    ext4_into();
+    match ext4_into() {
+        Ok(t) => t,
+        Err(e) => eprintln!("{}", e),
+    };
 }
--- rs-libblkid/src/lib.rs
+++ rs-libblkid/src/lib.rs
@@ -1,32 +1,99 @@
 use std::u16;
 use std::fs::File;
-use std::io::{LineWriter, Read, Seek, SeekFrom};
+use std::io::{Read, Seek, SeekFrom};
 use std::io::Cursor;
 use byteorder::{LittleEndian, ReadBytesExt};
+use uuid::Uuid;
 
+#[derive(Debug)]
 pub enum SuperState {
-    CleanlyUnmounted = 0x0001,   // Cleanly unmounted
-    ErrorsDetected = 0x0002,      // Errors detected
-    OrphansBeingRecovered = 0x0004, // Orphans being recovered
+    CleanlyUnmounted ,  
+    ErrorsDetected,      
+    OrphansBeingRecovered,
+    Unknown,
 }
 
+#[derive(Debug)]
 pub enum SuperErrors {
-    Continue = 1, // Continue
-    RemountRO = 2, // Remount read-only
-    Panic = 3, // Panic
+    Continue, 
+    RemountRO, 
+    Panic,
+    Unknown, 
 }
 
+#[derive(Debug)]
 pub enum SuperCreator {
-    Linux = 0,
-    Hurd = 1,
-    Masix = 2, 
-    FreeBSD = 3,
-    Lites = 4,
+    Linux,
+    Hurd,
+    Masix, 
+    FreeBSD,
+    Lites,
+    Unknown,
 }
 
+#[derive(Debug)]
 pub enum SuperRevision {
-    OriginalFormat = 1, // Original format
-    V2Format = 2, // v2 format w/ dynamic inode sizes
+    OriginalFormat,
+    V2Format,
+    Unknown,
+}
+
+#[derive(Debug)]
+pub enum SuperCompat {
+    CompatDirPrealloc,
+    CompatImagicInodes,
+    CompatHasJournal,
+    CompatExtAttr,
+    CompatResizeInode,
+    CompatDirIndex,
+    CompatLazyBg,
+    CompatExcludeInode,
+    CompatExcludeBitmap,
+    CompatSparseSuper2,
+    CompatFastCommit,
+    RoCompatOrphanPresent,
+    Unknown,
+}
+
+#[derive(Debug)]
+pub enum SuperIncompat {
+    IncompatCompression,
+    IncompatFiletype,
+    IncompatRecover,
+    IncompatJournalDev,
+    IncompatMetaBg,
+    IncompatExtents,
+    Incompat64bit,
+    IncompatMmp,
+    IncompatFlexBg,
+    IncompatEaInode,
+    IncompatDirdata,
+    IncompatCsumSeed,
+    IncompatLargeDir,
+    IncompatInlineData,
+    IncompatEncrypt,
+    Unknown,
+}
+
+#[derive(Debug)]
+pub enum SuperROCompat {
+    ROCompatSparseSuper,
+    RoCompatLargeFile,
+    RoCompatBtreeDir,
+    RoCompatHugeFile,
+    RoCompatGdtCsum,
+    RoCompatDirNlink,
+    RoCompatExtraIsize,
+    RoCompatHasSnapshot,
+    RoCompatQuota,
+    RoCompatBigalloc,
+    RoCompatMetadataCsum,
+    RoCompatReplica,
+    RoCompatReadonly,
+    RoCompatProject,
+    RoCompatVerity,
+    RoCompatOrphanPresent,
+    Unknown,
 }
 
 #[derive(Debug)]
@@ -47,29 +114,29 @@ pub struct Ext4SuperBlock {
     pub s_mnt_count: u16, 
     pub s_max_mnt_count: u16,
     pub s_magic: u16, 
-    pub s_state: u16, 
-    pub s_errors: u16, 
-    pub s_minor_rev_level: u16, 
+    pub s_state: SuperState, 
+    pub s_errors: SuperErrors, 
+    pub s_minor_rev_level: SuperRevision, 
     pub s_lastcheck: u32, 
     pub s_checkinterval: u32, 
-    pub s_creator_os: u32, 
+    pub s_creator_os: SuperCreator, 
     pub s_rev_level: u32, 
     pub s_def_resuid: u16, 
     pub s_def_resgid: u16, 
     pub s_first_ino: u32, 
     pub s_inode_size: u16, 
     pub s_block_group_nr: u16, 
-    pub s_feature_compat: u32,
-    pub s_feature_incompat: u32,
-    pub s_feature_ro_compat: u32,
-    pub s_uuid: [u8; 16],
+    pub s_feature_compat: SuperCompat,
+    pub s_feature_incompat: SuperIncompat,
+    pub s_feature_ro_compat: SuperROCompat,
+    pub s_uuid: Uuid,
     pub s_volume_name: [u8; 16], 
     pub s_last_mounted: [u8; 64],
     pub s_algorithm_usage_bitmap: u32,
     pub s_prealloc_blocks: u8,
     pub s_prealloc_dir_blocks: u8,
     pub s_reserved_gdt_blocks: u16,
-    pub s_journal_uuid: [u8; 16],
+    pub s_journal_uuid: Uuid,
     pub s_journal_inum: u32, 
     pub s_journal_dev: u32, 
     pub s_last_orphan: u32,
@@ -138,6 +205,7 @@ pub struct Ext4SuperBlock {
 
 pub fn read_ext4(device: &str) -> Result<Ext4SuperBlock, Box<dyn std::error::Error>>{
     let mut superblock = File::open(device)?;
+    // Move cursor forward 1024 bytes not to break GPT/MBR stuff
     superblock.seek(SeekFrom::Start(1024))?;
     let mut buffer = [0; 1024];
     superblock.read_exact(&mut buffer)?;
@@ -162,12 +230,45 @@ pub fn read_ext4(device: &str) -> Result<Ext4SuperBlock, Box<dyn std::error::Err
             s_mnt_count: rdr.read_u16::<LittleEndian>()?, 
             s_max_mnt_count: rdr.read_u16::<LittleEndian>()?,
             s_magic: rdr.read_u16::<LittleEndian>()?, 
-            s_state: rdr.read_u16::<LittleEndian>()?, 
-            s_errors: rdr.read_u16::<LittleEndian>()?, 
-            s_minor_rev_level: rdr.read_u16::<LittleEndian>()?, 
+            s_state: {  
+                let state = rdr.read_u16::<LittleEndian>()?;
+                match state {
+                    0x0001 => SuperState::CleanlyUnmounted,
+                    0x0002 => SuperState::ErrorsDetected,
+                    0x0004 => SuperState::OrphansBeingRecovered,
+                    _ => SuperState::Unknown,
+                }
+            }, 
+            s_errors: {
+                let errors = rdr.read_u16::<LittleEndian>()?;
+                match errors {
+                    1 => SuperErrors::Continue,
+                    2 => SuperErrors::RemountRO,
+                    3 => SuperErrors::Panic,
+                    _ => SuperErrors::Unknown,
+                }
+            }, 
+            s_minor_rev_level: {
+                let revision = rdr.read_u16::<LittleEndian>()?;
+                match revision {
+                    1 => SuperRevision::OriginalFormat,
+                    2 => SuperRevision::V2Format,
+                    _ => SuperRevision::Unknown,
+                } 
+            },
             s_lastcheck: rdr.read_u32::<LittleEndian>()?, 
             s_checkinterval: rdr.read_u32::<LittleEndian>()?, 
-            s_creator_os: rdr.read_u32::<LittleEndian>()?, 
+            s_creator_os: {
+                let creator = rdr.read_u32::<LittleEndian>()?;
+                match creator {
+                    0 => SuperCreator::Linux,
+                    1 => SuperCreator::Hurd,
+                    2 => SuperCreator::Masix,
+                    3 => SuperCreator::FreeBSD,
+                    4 => SuperCreator::Lites,
+                    _ => SuperCreator::Unknown,
+                }
+            }, 
             s_rev_level: rdr.read_u32::<LittleEndian>()?, 
             s_def_resuid: rdr.read_u16::<LittleEndian>()?, 
             s_def_resgid: rdr.read_u16::<LittleEndian>()?, 
@@ -180,7 +281,11 @@ pub fn read_ext4(device: &str) -> Result<Ext4SuperBlock, Box<dyn std::error::Err
             s_uuid: {
                 let mut uuid = [0u8; 16];
                 rdr.read_exact(&mut uuid)?;
-                uuid
+                let hex_string: String = uuid
+                    .iter()
+                    .map(|&b| format!("{:02x}", b))
+                    .collect();
+                Uuid::try_parse(&hex_string)?
             },
             s_volume_name: {
                 let mut volume_name = [0u8; 16];
@@ -199,7 +304,11 @@ pub fn read_ext4(device: &str) -> Result<Ext4SuperBlock, Box<dyn std::error::Err
             s_journal_uuid: {
                 let mut journal_uuid = [0u8; 16];
                 rdr.read_exact(&mut journal_uuid)?;
-                journal_uuid
+                let hex_string: String = journal_uuid
+                    .iter()
+                    .map(|&b| format!("{:02x}", b))
+                    .collect();
+                Uuid::try_parse(&hex_string)?
             },            
             s_journal_inum: rdr.read_u32::<LittleEndian>()?, 
             s_journal_dev: rdr.read_u32::<LittleEndian>()?, 
@@ -321,23 +430,3 @@ pub fn read_ext4(device: &str) -> Result<Ext4SuperBlock, Box<dyn std::error::Err
         }
     );
 }
-
-//pub fn read_ext4(device: &str) -> Result<(), Box<dyn std::error::Error>>{
-//    let mut superblock = File::open(device)?;
-//    superblock.seek(SeekFrom::Start(1024))?;
-//    let mut buffer = [0; 1024];
-//    superblock.read_exact(&mut buffer)?;
-//    
-//    for (i, byte) in buffer.iter().enumerate() {
-//        print!("{:02x} ", byte);
-//        if (i + 1) % 16 == 0 {
-//            println!(); // new line every 16 bytes
-//        }
-//    }
-//
-//    let bytes = [0x1d, 0xf5, 0x11, 0x68];
-//    let num = u32::from_le_bytes(bytes);   
-//    println!("First 4 bytes: {}", num);  
-//
-//    return Ok(());
-//}
