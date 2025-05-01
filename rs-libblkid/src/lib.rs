3e91b3019ee4b4cb2a0b006fc0b27db0d33050f1
rysndavjd <138254890+rysndavjd@users.noreply.github.com>
2025-05-01 20:27:26 +0200

    generic mbr

--- rs-libblkid/src/lib.rs
+++ rs-libblkid/src/lib.rs
@@ -1,2 +1,12 @@
 pub mod mbr;
 pub mod ext4;
+
+//pub fn test(device: &str) -> Result<(), Box<dyn std::error::Error>> {
+//    let mut superblock = File::open(device)?;
+//    let mut buffer = [0; 512];
+//    superblock.read_exact(&mut buffer)?;
+//
+//    println!("{:X?}", buffer);
+//    return Ok(());
+//
+//}
