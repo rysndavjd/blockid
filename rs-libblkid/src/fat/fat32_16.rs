14eb12ab34739882e1e8dddd1c667d98f33d5fbf
rysndavjd <rysndavjd@proton.me>
2025-05-04 00:50:45 +0200

    more fat

--- rs-libblkid/src/fat/fat32_16.rs
+++ rs-libblkid/src/fat/fat32_16.rs
@@ -1,21 +1,11 @@
-use thiserror::Error;
-use std::str::Utf8Error;
-use byteorder::{ByteOrder, LittleEndian};
 use std::u16;
 use std::fs::File;
 use std::io::Read;
+use byteorder::{ByteOrder, LittleEndian};
 use bytemuck::checked::from_bytes;
 use bytemuck::{Pod, Zeroable};
 use arrayref::array_ref;
 
-#[derive(Debug)]
-pub enum FatType {
-    ExFat,
-    Fat32,
-    Fat16,
-    Fat12,
-    Unknown
-}
 
 #[repr(C, packed)]
 #[derive(Debug, Clone, Copy, Pod, Zeroable)]
@@ -54,7 +44,6 @@ pub struct RawFatExtBs16 {
     pub head_side_count: u16,
     pub hidden_sector_count: u32,
     pub total_sectors_32: u32,
-
     pub bios_drive_num: u8,
     pub windows_nt_flags: u8,
     pub boot_signature: u8,
@@ -116,8 +105,7 @@ pub struct Fat16Header {
     pub head_side_count: u16,
     pub hidden_sector_count: u32,
     pub total_sectors_32: u32,
-    pub extended_section: [u8; 476],
-    pub boot_sector: [u8; 36],
+
     pub bios_drive_num: u8,
     pub windows_nt_flags: u8,
     pub boot_signature: u8,
@@ -197,6 +185,47 @@ impl From<RawFatBS> for RawFatExtBs32 {
     }
 }
 
+impl From<RawFatExtBs32> for Fat32Header {
+    fn from(raw: RawFatExtBs32) -> Self {
+        Fat32Header {
+            bootjmp: raw.bootjmp,
+            oem_name: {
+                String::from_utf8_lossy(&raw.oem_name).to_string()
+            },
+            bytes_per_sector: raw.bytes_per_sector,
+            sectors_per_cluster: raw.sectors_per_cluster,
+            reserved_sector_count: raw.reserved_sector_count,
+            table_count: raw.table_count,
+            root_entry_count: raw.root_entry_count,
+            total_sectors_16: raw.total_sectors_16,
+            media_type: raw.media_type,
+            table_size_16: raw.table_size_16,
+            sectors_per_track: raw.sectors_per_track,
+            head_side_count: raw.head_side_count,
+            hidden_sector_count: raw.hidden_sector_count,
+            total_sectors_32: raw.total_sectors_32,
+            table_size_32: raw.table_size_32,
+            extended_flags: raw.extended_flags,
+            fat_version: raw.fat_version,
+            root_clustor: raw.root_clustor,
+            fat_info: raw.fat_info,
+            backup_bs_sector: raw.backup_bs_sector,
+            reserved_0: raw.reserved_0,
+            drive_number: raw.drive_number,
+            windows_nt_flags: raw.windows_nt_flags,
+            boot_signature: raw.boot_signature,
+            volume_id: raw.volume_id,
+            volume_label: {
+                String::from_utf8_lossy(&raw.volume_label).to_string()
+            },
+            fat_type_label: {
+                String::from_utf8_lossy(&raw.fat_type_label).to_string()
+            },
+            executable_code: raw.executable_code,
+            boot_flag: raw.boot_flag,
+        }
+    }
+}
 
 impl From<RawFatBS> for RawFatExtBs16 {
     fn from(raw: RawFatBS) -> Self {
@@ -227,45 +256,71 @@ impl From<RawFatBS> for RawFatExtBs16 {
     }
 }
 
-
-pub fn fat_type(boot_sector: RawFatBS) -> Result<FatType, Box<dyn std::error::Error>> {
-    
-    if boot_sector.bytes_per_sector == 0 {
-        return Ok(FatType::ExFat);
+impl From<RawFatExtBs16> for RawFatBS {
+    fn from(raw: RawFatExtBs16) -> Self {
+        RawFatBS {
+            bootjmp: raw.bootjmp,
+            oem_name: raw.oem_name,
+            bytes_per_sector: raw.bytes_per_sector,
+            sectors_per_cluster: raw.sectors_per_cluster,
+            reserved_sector_count: raw.reserved_sector_count,
+            table_count: raw.table_count,
+            root_entry_count: raw.root_entry_count,
+            total_sectors_16: raw.total_sectors_16,
+            media_type: raw.media_type,
+            table_size_16: raw.table_size_16,
+            sectors_per_track: raw.sectors_per_track,
+            head_side_count: raw.head_side_count,
+            hidden_sector_count: raw.hidden_sector_count,
+            total_sectors_32: raw.total_sectors_32,
+            extended_section: {
+                let mut extended_section: [u8; 476] = [0; 476];
+                extended_section[0] = raw.bios_drive_num;
+                extended_section[1] = raw.windows_nt_flags;
+                extended_section[2] = raw.boot_signature;
+                LittleEndian::write_u32(&mut extended_section[2..6], raw.volume_id);
+                extended_section[8..19].copy_from_slice(&raw.volume_label);
+                extended_section[19..27].copy_from_slice(&raw.fat_type_label);
+                extended_section[27..475].copy_from_slice(&raw.executable_code);
+                LittleEndian::write_u16(&mut extended_section[475..476], raw.boot_flag);
+                extended_section
+            },
+        }
     }
+}
 
-    let boot_sector_fat32: RawFatExtBs32 = boot_sector.into();  
-    
-    let total_sectors = if boot_sector.total_sectors_16 == 0 {
-        boot_sector.total_sectors_32
-    } else {
-        boot_sector.total_sectors_16 as u32
-    };
-    
-    let fat_size = if boot_sector.table_size_16 == 0 {
-        boot_sector_fat32.table_size_32
-    } else {
-        boot_sector.table_size_16 as u32
-    };
-
-    let root_dir_sectors = ((boot_sector.root_entry_count as u32 * 32)
-                                + (boot_sector.bytes_per_sector as u32 - 1) ) // I know this panics here if bytes_per_sector equal 0
-                                / boot_sector.bytes_per_sector as u32;
-
-    let first_data_sector = boot_sector.reserved_sector_count as u32
-                                + (boot_sector.table_count as u32 * fat_size)
-                                + root_dir_sectors;
-
-    let data_sectors = total_sectors - first_data_sector;
-    
-    let total_clusters = data_sectors / boot_sector.sectors_per_cluster as u32;
-    
-    if total_clusters < 4085 {
-        return Ok(FatType::Fat12);
-    } else if total_clusters < 65525 {
-        return Ok(FatType::Fat16);
-    } else {
-        return Ok(FatType::Fat32);
+impl From<RawFatExtBs16> for Fat16Header {
+    fn from(raw: RawFatExtBs16) -> Self {
+        Fat16Header {
+            bootjmp: raw.bootjmp,
+            oem_name: {
+                String::from_utf8_lossy(&raw.oem_name).to_string()
+            },
+            bytes_per_sector: raw.bytes_per_sector,
+            sectors_per_cluster: raw.sectors_per_cluster,
+            reserved_sector_count: raw.reserved_sector_count,
+            table_count: raw.table_count,
+            root_entry_count: raw.root_entry_count,
+            total_sectors_16: raw.total_sectors_16,
+            media_type: raw.media_type,
+            table_size_16: raw.table_size_16,
+            sectors_per_track: raw.sectors_per_track,
+            head_side_count: raw.head_side_count,
+            hidden_sector_count: raw.hidden_sector_count,
+            total_sectors_32: raw.total_sectors_32,
+            bios_drive_num: raw.bios_drive_num,
+            windows_nt_flags: raw.windows_nt_flags,
+            boot_signature: raw.boot_signature,
+            volume_id: raw.volume_id,
+            volume_label: {
+                String::from_utf8_lossy(&raw.volume_label).to_string()
+            },
+            fat_type_label: {
+                String::from_utf8_lossy(&raw.fat_type_label).to_string()
+            },
+            executable_code: raw.executable_code,
+            boot_flag: raw.boot_flag,
+        }
     }
 }
 
@@ -275,33 +330,41 @@ pub fn read_raw_fat_bs(device: &str) -> Result<RawFatBS, Box<dyn std::error::Err
     
     raw.read_exact(&mut buffer)?;
     
-    Ok(*from_bytes::<RawFatBS>(&buffer))
+    return Ok(*from_bytes::<RawFatBS>(&buffer));
 }
 
-pub fn read_raw_fat16_ext_bs(device: &str) -> Result<RawFatExtBs16, Box<dyn std::error::Error>> {
+pub fn read_raw_fat16_bs(device: &str) -> Result<RawFatExtBs16, Box<dyn std::error::Error>> {
     let mut raw = File::open(device)?;
     let mut buffer = [0u8; 512];
     
     raw.read_exact(&mut buffer)?;
     
-    Ok(*from_bytes::<RawFatExtBs16>(&buffer))
+    return Ok(*from_bytes::<RawFatExtBs16>(&buffer));
 }
 
-pub fn read_raw_fat32_ext_bs(device: &str) -> Result<RawFatExtBs32, Box<dyn std::error::Error>> {
+pub fn read_raw_fat32_bs(device: &str) -> Result<RawFatExtBs32, Box<dyn std::error::Error>> {
     let mut raw = File::open(device)?;
     let mut buffer = [0u8; 512];
     
     raw.read_exact(&mut buffer)?;
     
-    Ok(*from_bytes::<RawFatExtBs32>(&buffer))
+    return Ok(*from_bytes::<RawFatExtBs32>(&buffer));
 }
 
+pub fn read_fat16_bs(device: &str) -> Result<Fat16Header, Box<dyn std::error::Error>> {
+    let mut raw = File::open(device)?;
+    let mut buffer = [0u8; 512];
+    
+    raw.read_exact(&mut buffer)?;
+    
+    return Ok((*from_bytes::<RawFatExtBs16>(&buffer)).into());
+}
 
-pub fn tests(device: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut superblock = File::open(device)?;
-    let mut buffer = [0; 512];
-    superblock.read_exact(&mut buffer)?;
-
-    println!("{:X?}", buffer);
-    return Ok(());
+pub fn read_fat32_bs(device: &str) -> Result<Fat32Header, Box<dyn std::error::Error>> {
+    let mut raw = File::open(device)?;
+    let mut buffer = [0u8; 512];
+    
+    raw.read_exact(&mut buffer)?;
+    
+    return Ok((*from_bytes::<RawFatExtBs32>(&buffer)).into());
 }
