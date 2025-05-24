e9789d15aa412cb3e00829fc3090042293d35666
rysndavjd <138254890+rysndavjd@users.noreply.github.com>
2025-05-24 19:27:44 +0200

    kinda works need to change stuff to actual rust not weird c rust

--- blockid/src/main.rs
+++ blockid/src/main.rs
@@ -14,8 +14,28 @@ fn test() -> Result<(), Box<dyn std::error::Error>> {
     //let ms = read_as_msdos("/home/rysndavjd/github/blockid/sample-headers/fat12.bin")?;
     //
     //let device = fat_type(vs, ms)?;
-    let device = probe_is_vfat(&File::open("/home/rysndavjd/github/blockid/sample-headers/fat16.bin")?)?;
-    println!("{:?}", device);
+
+    let ProbeResults: ProbeResults = ProbeResults{
+        fs_type: None,
+        sec_type: None,
+        uuid: None,
+        uuid_sub: None,
+        label: None,
+        label_raw: None,
+        fs_version: None,
+        usage: None,
+        part_uuid: None,
+        part_name: None,
+        part_number: None,
+        part_scheme: None
+    };
+
+    let mut probe: BlockProbe = BlockProbe { file: File::open("/dev/sdb3")?,
+                                        begin: 0, end: 0, devno: 0, disk_devno: 0, probe_flags: BlkidFlags::empty(), values: ProbeResults};
+
+    let magic = probe_get_magic(&mut probe, &VFAT_ID_INFO)?;
+    let device = probe_vfat(&mut probe, magic)?;
+    //println!("{:?}", device);
 
     return Ok(());
 }
