5d465db1e7d9b41191d8f5ccb7a5f15b867a8c27
rysndavjd <138254890+rysndavjd@users.noreply.github.com>
2026-04-06 23:10:41 +0200

    move everthing

--- blockid/build.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-
-fn main() {
-    let path = env::var("CACHE_PATH")
-        .unwrap_or(String::from("/tmp/blockid.tab"));
-
-    println!("cargo:warning=Using cache path: \"{path}\"");
-    println!("cargo:rustc-env=CACHE_PATH={path}");
-}
